def5d29d4b3819b9bb560a3644983c848139c8ba
AaaBin <birnie1571@gmail.com>
2020-04-11 01:45:54 +0800

    2020-04-11-0145

--- 03-JS/js.js
+++ 03-JS/js.js
@@ -1,7 +1,9 @@
+
+
+// 首頁
 let landing_img1 = document.querySelector('.landing_img1');
 let landing_img2 = document.querySelector('.landing_img2');
 let landing_img3 = document.querySelector('.landing_img3');
-
 setTimeout(function (params) {
     landing_img1.setAttribute('style','left:-70px')
     landing_img2.setAttribute('style','left:70px')
