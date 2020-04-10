03c66adb77dfc36194242d08d3d7c9eeae53157b
AaaBin <birnie1571@gmail.com>
2020-04-11 00:27:08 +0800

    2020-04-11-0027

---
+++ 03-JS/js.js
@@ -0,0 +1,8 @@
+let landing_img1 = document.querySelector('.landing_img1');
+let landing_img2 = document.querySelector('.landing_img2');
+let landing_img3 = document.querySelector('.landing_img3');
+
+setTimeout(function (params) {
+    landing_img1.setAttribute('style','left:-70px')
+    landing_img2.setAttribute('style','left:70px')
+},1000)
