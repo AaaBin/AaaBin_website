1c755964ef0b9a5dcad191d0b2552200c0f6645f
AaaBin <birnie1571@gmail.com>
2020-04-11 19:22:32 +0800

    2020-04-11-1922

--- 03-JS/js.js
+++ 03-JS/js.js
@@ -3,23 +3,27 @@ new fullpage('#fullpage', {
     //options here
     autoScrolling: true,
     scrollHorizontally: true,
-    navigation: true,
     showActiveTooltip: true,
     navigationTooltips: ['WELCOME', 'PROFILE', 'SKILL', 'PROJECT', 'NOTE', 'PHOTO'],
     dragAndMove: true,
+    anchors:['WELCOME', 'PROFILE', 'SKILL','PROJECT','NOTE']
 });
+
+// AOS初始化
+AOS.init();
+
 
 //methods
 // fullpage_api.setAllowScrolling(false);
 
 // 首頁
-let landing_img1 = document.querySelector('.landing_img1');
-let landing_img2 = document.querySelector('.landing_img2');
-let landing_img3 = document.querySelector('.landing_img3');
-setTimeout(function (params) {
-    landing_img1.setAttribute('style', 'left:-70px')
-    landing_img2.setAttribute('style', 'left:70px')
-}, 1000)
+// let landing_img1 = document.querySelector('.landing_img1');
+// let landing_img2 = document.querySelector('.landing_img2');
+// let landing_img3 = document.querySelector('.landing_img3');
+// setTimeout(function (params) {
+//     landing_img1.setAttribute('style', 'left:-70px')
+//     landing_img2.setAttribute('style', 'left:70px')
+// }, 1000)
 
 // skill collapse
 $('#fullpage').on('show.bs.collapse', function () {
