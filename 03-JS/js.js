d294c1c7fca5ee89c2ebe70345eca29c95227c60
AaaBin <birnie1571@gmail.com>
2020-04-11 16:12:15 +0800

    2020-04-11-1612

--- 03-JS/js.js
+++ 03-JS/js.js
@@ -1,10 +1,26 @@
+// fullpage初始化
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    navigation: true,
+    showActiveTooltip: true,
+    navigationTooltips: ['WELCOME', 'PROFILE', 'SKILL', 'PROJECT', 'NOTE', 'PHOTO'],
+});
 
+//methods
+// fullpage_api.setAllowScrolling(false);
 
 // 首頁
 let landing_img1 = document.querySelector('.landing_img1');
 let landing_img2 = document.querySelector('.landing_img2');
 let landing_img3 = document.querySelector('.landing_img3');
 setTimeout(function (params) {
-    landing_img1.setAttribute('style','left:-70px')
-    landing_img2.setAttribute('style','left:70px')
-},1000)
+    landing_img1.setAttribute('style', 'left:-70px')
+    landing_img2.setAttribute('style', 'left:70px')
+}, 1000)
+
+// skill collapse
+$('#fullpage').on('show.bs.collapse', function () {
+    $('.collapse').collapse('hide');
+})
